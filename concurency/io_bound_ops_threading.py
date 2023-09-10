dd17daf704f65ec0cde9aabbdebc0e3dabace6ac
Nikodem Kirsz <nikodem.kirsz@gmail.com>
2023-09-10 11:17:16 +0200

    initiate

--- concurency/io_bound_ops_threading.py
+++ concurency/io_bound_ops_threading.py
@@ -10,6 +10,28 @@ import concurrent.futures
 # multiprocessing (running on 8 cores on CPU) took 2,19 seconds
 # multiprocessing (running on 30 processes and 8 cores on CPU) took 1,11 seconds
 
+"""
+
+Parallelism consists of performing multiple operations at the same time. 
+Multiprocessing is a means to effect parallelism, and it entails spreading 
+tasks over a computer’s central processing units (CPUs, or cores). 
+Multiprocessing is well-suited for CPU-bound tasks: tightly bound for loops 
+and mathematical computations usually fall into this category.
+
+Concurrency is a slightly broader term than parallelism. 
+It suggests that multiple tasks have the ability to run in an overlapping manner. (There’s a saying that concurrency does not imply parallelism.)
+
+Threading is a concurrent execution model whereby multiple threads take turns 
+executing tasks. One process can contain multiple threads. 
+
+                    Concurency
+    (tasks have ability to run in overlapping manner)
+                     /
+                  *--Parralelism---*
+                   /          /
+    Multiprocessing(CPU)   Threading(I/O)  
+                 
+"""
 thread_local = threading.local()
 
 def get_session():
